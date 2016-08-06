ae604aa621541b14e33f7d0b9d3c73992b9a597d
Björn Ritzl <bjorn.ritzl@king.com>
2016-08-06 10:22:33 +0200

    Adjusted collision shapes. Added bubbles.

--- game/fish/fish_data.lua
+++ game/fish/fish_data.lua
@@ -4,12 +4,14 @@ return {
 		score_factor = 10,
 		frequency = 250,
 		depth = { min = -500, max = -1500 },
+		factory = "#blackhead_factory",
 	}, 
 	[2] = {
 		name = "Triangle fish",
 		score_factor = 10,
 		frequency = 100,
 		depth = { min = -750, max = -1750 },
+		factory = "#trianglefish_factory",
 	}, 
 	[3] = {
 		name = "Jelly fish",
@@ -19,30 +21,35 @@ return {
 		distance_factor = 0.01,
 		depth_factor = 4,
 		speed_factor = 2,
+		factory = "#jellyfish_factory",
 	}, 
 	[4] = {
 		name = "Fat fish",
 		score_factor = 10,
 		frequency = 200,
 		depth = { min = -250, max = -1000 },
+		factory = "#fatfish_factory",
 	}, 
 	[5] = {
 		name = "Small fish",
 		score_factor = 10,
 		frequency = 1500,
 		depth = { min = -50, max = -700 },
+		factory = "#smallfish_factory",
 	},
 	[6] = {
-		name = "Tube fish",
+		name = "Barracuda",
 		score_factor = 10,
 		frequency = 25,
 		depth = { min = -950, max = -2000 },
+		factory = "#barracuda_factory",
 	}, 
 	[7] = {
 		name = "Star fish",
 		score_factor = 10,
 		frequency = 5,
 		depth = { min = -500, max = -2000 },
+		factory = "#starfish_factory",
 	}, 
 }
 
--- game/fish/smallfish.go
+++ game/fish/smallfish.go
@@ -1,6 +1,6 @@
 components {
   id: "script"
-  component: "/game/fish.script"
+  component: "/game/fish/fish.script"
   position {
     x: 0.0
     y: 0.0
@@ -40,8 +40,8 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 20.0\n"
-  "  data: 10.0\n"
+  "  data: 19.0\n"
+  "  data: 8.5\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -74,7 +74,7 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: -15.0\n"
+  "      x: -10.0\n"
   "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -109,7 +109,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/fishing.atlas\"\n"
-  "default_animation: \"fish1\"\n"
+  "default_animation: \"fish5\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
