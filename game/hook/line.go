66a30a8bd00a18bcc0f167383f75bf4b7b84cf6e
Björn Ritzl <bjorn.ritzl@king.com>
2017-02-17 14:05:43 +0100

    Removed unused stuff

--- game/hook/line.go
+++ game/hook/line.go
@@ -14,23 +14,6 @@ components {
   }
 }
 embedded_components {
-  id: "factory"
-  type: "factory"
-  data: "prototype: \"/game/hook/line_segment.go\"\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/fishing.atlas\"\n"
